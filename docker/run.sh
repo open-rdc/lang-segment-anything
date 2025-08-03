6493053a7e78df9461c36cfdde82fdfb8992a3d1
yasuohayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2025-08-03 10:21:03 +0900

    modify run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
-docker run --gpus all -p 8000:8000 lang-segment-anything:latest &
-xdg-open http://localhost:8000/gradio
+xdg-open http://localhost:8000/gradio &
+docker run --gpus all -p 8000:8000 lang-segment-anything:latest
